50067278c81baa1e1ea64a9c5ca761a59a45e737
Trongddan <doandan301@gmail.com>
2023-03-23 22:08:09 +0700

    ok

--- src/config/db/DBconnection.js
+++ src/config/db/DBconnection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Database = {
-  connect:()=>{ mongoose.connect("mongodb://localhost:27017/football").then(()=>console.log("kết nối thành công")).catch((err)=>console.log(err))
+  connect:()=>{ mongoose.connect("mongodb+srv://dankenvil:dankenvil@cluster0.hihao2c.mongodb.net/?retryWrites=true&w=majority").then(()=>console.log("kết nối thành công")).catch((err)=>console.log(err))
    
 }
 }
